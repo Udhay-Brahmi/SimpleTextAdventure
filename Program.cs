7b0e98c6c2cd68bbf0b309ce00bae3bf6ef36065
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2020-10-31 20:58:37 -0500

    I'm back from a power outage (hopefully)

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace SimpleTextAdventure
     {
         public static string gameName = "SimpleTextAdventure";
         public static string gameAuthor = "Nonparoxysmic";
-        public static string gameVersion = "Alpha 0.1_8";
+        public static string gameVersion = "Alpha 0.1_9";
 
         static readonly string wrappingIndent = "  ";
         static readonly bool indentFirstLine = false;
